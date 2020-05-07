b5ecd19edd6d8c2c1db9bee2194eeb77620c8ebc
mai cook <mailiecook@yahoo.com>
2020-05-07 16:19:14 -0700

    added leveling up and leveling down functionality

--- main.cs
+++ main.cs
@@ -8,6 +8,7 @@ namespace PlantCare
   {
     public static void Main()
     {
+   
       List<PlantMakeup> Plant = new List<PlantMakeup>();
 
       Console.WriteLine("What type of plant do you want to take care of?");
@@ -17,13 +18,13 @@ namespace PlantCare
       Console.WriteLine("What do you want to name your plant?");
       string plantName = Console.ReadLine();
 
-      Plant.Add(new PlantMakeup(plantType, plantName, "seed", 10));
+      Plant.Add(new PlantMakeup(plantType, plantName, "seed"));
 
       Console.WriteLine("Your plant's type is: " + plantType);
       Console.WriteLine("Your plant's name is: " + plantName);
       Console.WriteLine(plantName + " is currently a seed.");
       Console.WriteLine("What action do you want to do?");
-      Console.WriteLine("water || feed || sunshine || weed || neglect || quit");
+      Console.WriteLine("water || feed || sunshine || weed || read || quit");
       string action = Console.ReadLine();
 
       while (action != "quit" || action != "Quit")
@@ -31,7 +32,7 @@ namespace PlantCare
         Console.WriteLine("ACTION 1****************");
         Console.WriteLine(action);
         Console.WriteLine("----------------");
-
+    
         if (action == "water" || action == "Water")
         {
           Console.WriteLine(PlantMakeup.WaterPlant());
@@ -52,12 +53,12 @@ namespace PlantCare
           Console.WriteLine(PlantMakeup.WeedPlant());
           Console.WriteLine("----------------");
         }
-        else if (action == "neglect" || action == "Neglect")
+        else if (action == "read" || action == "Read")
         {
-          Console.WriteLine(PlantMakeup.NeglectPlant());
+          Console.WriteLine(PlantMakeup.ReadPlant());
           Console.WriteLine("----------------");
         }
-        else if (action == "quit" || action == "Quit")
+         else if (action == "quit" || action == "Quit")
         {
           Console.WriteLine("Goodbye.");
           break;
@@ -67,9 +68,38 @@ namespace PlantCare
           Console.WriteLine("I did not understand, please type one of the actions.");
           Console.WriteLine("----------------");
         }
+
+        if (PlantMakeup.CareStats["water"] == 1 && PlantMakeup.CareStats["sun"] == 1)
+        {
+          Console.WriteLine(plantName + " is now a seedling!");
+        }
+        else if (PlantMakeup.CareStats["water"] == 2 && PlantMakeup.CareStats["sun"] == 2 && PlantMakeup.CareStats["weed"] == 1)
+        {
+          Console.WriteLine(plantName + " is now a teen plant! You found them listening to MCR!");
+        }
+        else if (PlantMakeup.CareStats["water"] == 3 && PlantMakeup.CareStats["sun"] == 3 && PlantMakeup.CareStats["weed"] == 2 && PlantMakeup.CareStats["feed"] == 1)
+        {
+          Console.WriteLine(plantName + " is now having a growth spurt!");
+        }
+        else if (PlantMakeup.CareStats["water"] == 4 && PlantMakeup.CareStats["sun"] == 3 && PlantMakeup.CareStats["weed"] == 2 && PlantMakeup.CareStats["feed"] == 2)
+        {
+          Console.WriteLine(plantName + " is now flowering!");
+        }
+        else if (PlantMakeup.CareStats["health"] == 5)
+        {
+          Console.WriteLine(plantName + " is wilting!");
+        }
+        else if (PlantMakeup.CareStats["health"] == 0)
+        {
+          Console.WriteLine(plantName + " is dead!");
+          break;
+        }
+       
+        
+        
         System.Threading.Thread.Sleep(2000);
         Console.WriteLine("What action do you want to do?");
-        Console.WriteLine("water || feed || sunshine || weed || neglect || quit");
+        Console.WriteLine("water || feed || sunshine || weed || read || quit");
         action = Console.ReadLine();
       }
 
--- plant.cs
+++ plant.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace PlantCare
 {
@@ -7,37 +8,70 @@ namespace PlantCare
     public string PlantName { get; set; }
     public string PlantType { get; set; }
     public string PlantAge { get; set; }
-    public int PlantHealth { get; set;}
-
-    public static string WaterPlant()
-    {
-      return "Your plant has been watered!";
-    }
-    public static string FeedPlant()
-    {
-      return "Your plant has been feed a fly!";
-    }
-    public static string SunPlant()
-    {
-      return "Your plant has felt the warmth of the sun!";
-    }
-    public static string WeedPlant()
-    {
-      return "Your plant has gotten a haircut!";
-    }
-    public static string NeglectPlant()
-    {
-      return "Your plant is lonely :(";
-    }
-    public PlantMakeup(string plantName, string plantType, string plantAge, int plantHealth)
+   
+   
+    public static Dictionary<string, int> CareStats;
+    public PlantMakeup(string plantName, string plantType, string plantAge)
     {
       PlantName = plantName;
       PlantType = plantType;
       PlantAge = plantAge;
-      PlantHealth = plantHealth;
+      CareStats =  new Dictionary<string, int>();
+      CareStats.Add("water", 0);
+      CareStats.Add("sun", 0);
+      CareStats.Add("feed", 0);
+      CareStats.Add("weed", 0);
+      CareStats.Add("health", 10);
     }
+      
+    public static string WaterPlant()
+    {
+      
+      CareStats["water"] ++;
+      CareStats["health"] ++;
+      return "Your plant has been watered!" + " " + CareStats["water"];
+    }
+    public static string FeedPlant()
+    {
+      CareStats["feed"] ++;
+      CareStats["health"] ++;
+      return "Your plant has been feed a fly! " + CareStats["feed"];
+    }
+    public static string SunPlant()
+    {
+      CareStats["sun"] ++;
+      CareStats["health"] ++;
+      return "Your plant has felt the warmth of the sun! " + CareStats["sun"];
+    }
+    public static string WeedPlant()
+    {
+      CareStats["weed"] ++;
+      CareStats["health"] ++;
+      return "Your plant has gotten a haircut! " + CareStats["weed"];
+    }
+    public static string ReadPlant()
+    {
+      CareStats["health"] --;
+      return "Your took a break and read. It was relaxing. ";
+    }
+
+    
+    // public Dictionary<int, string> careStats = new Dictionary<int, string>();
+    // careStats.Add(0, "water");
+    // careStats.Add(0, "sun");
+    // careStats.Add(0, "feed");
+    // careStats.Add(0, "weed");
+
+
+
   }
 }
 
 // different types of plants require different types of care?
 // seed -> seedling -> teen plant (growth stage) -> vegetative state -> flowering stage -> reproduction stage -> regeneration stage
+// 1 water 1 sun, *name* is now a seedling! -> 2 water 2 sun 1 weed, *name* is now a young *plant*! -> 3 water 3 sun 2 weed 1 feed, *name* is now growing more! ->  4 water 3 sun 2 weed 2 feed, *name* is now flowering! -> 4 water 4 sun 3 weed 3 feed, *name* is now producing pollen! -> 4 water 4 sun 4 weed 4 feed, *name* is now producing seeds!
+// 7 stages
+// define each age parameter
+// each stage will wait until variables meet parameters
+// have counters for parameters that reset every time it grows
+// {[water, 0], [sun, 2], [weed, 1], [feed, 2]}
