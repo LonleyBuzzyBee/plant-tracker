fd67cce87b4394aae85e037547f54227a1f419c4
mai cook <mailiecook@yahoo.com>
2020-05-07 11:20:56 -0700

    add if statement for actions in main.cs, add methods for actions in plant.cs

--- main.cs
+++ main.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using PlantInfo;
+
 
 namespace PlantCare
 {
@@ -8,22 +8,57 @@ namespace PlantCare
   {
     public static void Main()
     {
-      List<PlantCare> Plant = new List<PlantCare>();
+      List<PlantMakeup> Plant = new List<PlantMakeup>();
 
       Console.WriteLine("What type of plant do you want to take care of?");
-      Console.WriteLine("Dandelion, Succulent, or Sunflower");
+      Console.WriteLine("Venus-Flytrap, Succulent, or Sunflower");
       string plantType = Console.ReadLine();
 
       Console.WriteLine("What do you want to name your plant?");
       string plantName = Console.ReadLine();
       
-      Plant.Add(new PlantCare(plantType,plantName, "seedling"));
+      Plant.Add(new PlantMakeup(plantType,plantName, "seed", 10));
+
       Console.WriteLine("Your plant's type is: " + plantType);
       Console.WriteLine("Your plant's name is: " + plantName);
       Console.WriteLine(plantName + " is currently a seed.");
       Console.WriteLine("What action do you want to do?");
-      Console.WriteLine("water || feed || sunshine || weed");
+      Console.WriteLine("water || feed || sunshine || weed || neglect || quit");
+      string action = Console.ReadLine();
+      while (action !)
+      if (action == "water" || action == "Water")
+      {
+        Console.WriteLine(PlantMakeup.WaterPlant());
+      }
+      else if (action == "feed" || action == "Feed")
+      {
+        Console.WriteLine(PlantMakeup.FeedPlant());
+      }
+      else if (action == "sunshine" || action == "Sunshine")
+      {
+        Console.WriteLine(PlantMakeup.SunPlant());
+      }
+      else if (action == "weed" || action == "Weed")
+      {
+        Console.WriteLine(PlantMakeup.WeedPlant());
+      }
+      else if (action == "neglect" || action == "Neglect")
+      {
+        Console.WriteLine(PlantMakeup.NeglectPlant());
+      }
+      else if (action == "quit" || action == "Quit")
+      {
+        return;
+      }
+      else
+      {
+        Console.WriteLine("I did not understand, please type one of the actions.")
+      }
 
     }
   }
 }
+    // public -> the access modifier (default is 'internal'), labels how this method is accessed. Public allows any part of internal or external code to access it.
+    // static -> allows access without instantiating the class, for example Main() must be static as Program can't be instantiated
+    // void -> return data type, void denotes returning nothing (i.e. no 'return' statement in the method), it can be any data type (string, int, int[](array of ints), bool, etc) but there can only be one return type
+    // Main() -> name of the method
--- plant.cs
+++ plant.cs
@@ -1,19 +1,43 @@
-namespace PlantInfo
+using System;
+
+namespace PlantCare
 {
   public class PlantMakeup
   {
-    public string PlantName {get; set; }
-    public string PlantType {get; set; }
-    public string PlantAge {get; set; }
+    public string PlantName { get; set; }
+    public string PlantType { get; set; }
+    public string PlantAge { get; set; }
+    public int PlantHealth { get; set;}
 
-    public PlantCare(string plantName, string plantType, string plantAge)
+    public static string WaterPlant()
+    {
+      return "Your plant has been watered!";
+    }
+    public static string FeedPlant()
+    {
+      return "Your plant has been feed a fly!";
+    }
+    public static string SunPlant()
+    {
+      return "Your plant has felt the warmth of the sun!";
+    }
+    public static string WeedPlant()
+    {
+      return "Your plant has gotten a haircut!";
+    }
+    public static string NeglectPlant()
+    {
+      return "Your plant is lonely :(";
+    }
+    public PlantMakeup(string plantName, string plantType, string plantAge, int plantHealth)
     {
       PlantName = plantName;
       PlantType = plantType;
       PlantAge = plantAge;
+      PlantHealth = plantHealth;
     }
-    
   }
 }
+
 // different types of plants require different types of care?
 // seed -> seedling -> teen plant (growth stage) -> vegetative state -> flowering stage -> reproduction stage -> regeneration stage
